05a58d0e49f714fafc6c0aef6615a7835d719653
Aukai Tirrell <aukaitirrell@webmocha.com>
2025-08-15 09:28:19 -0600

    update wireframe

--- src/ContentBlocks/BlockExternalLinks/BlockExternaLinksWireFrame.js
+++ src/ContentBlocks/BlockExternalLinks/BlockExternaLinksWireFrame.js
@@ -4,10 +4,10 @@ import Box from "grommet/components/Box";
 export default function BlockExternaLinksWireFrame() {
   return (
     <Box pad={{ between: "small" }}>
-      <Box full="horizontal" pad="medium" colorIndex="brand" />
-      <Box full="horizontal" pad="medium" colorIndex="brand" />
-      <Box full="horizontal" pad="medium" colorIndex="brand" />
-      <Box full="horizontal" pad="medium" colorIndex="brand" />
+      <Box full="horizontal" pad="small" colorIndex="brand" />
+      <Box full="horizontal" pad="small" colorIndex="brand" />
+      <Box full="horizontal" pad="small" colorIndex="brand" />
+      <Box full="horizontal" pad="small" colorIndex="brand" />
     </Box>
   );
 }
